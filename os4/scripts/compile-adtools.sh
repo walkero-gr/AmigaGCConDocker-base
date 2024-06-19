04f4b2de82c84bd25a004fe698957f1ab08ad2b5
George Sokianos <walkero@gmail.com>
2024-06-20 00:00:35 +0100

    Enabled some of the commants in the compile script

--- os4/scripts/compile-adtools.sh
+++ os4/scripts/compile-adtools.sh
@@ -1,15 +1,12 @@
 #!/usr/bin/bash
 
-echo "-------- START COMPILATION OF ADTOOLS"
-mkdir /opt/ppc-amigaos
+git config --global user.email "walkero@gmail.com"
+git config --global user.name "Georgios Sokianos"
+git submodule init && \
+	git submodule update && \
+	gild/bin/gild checkout binutils 2.23.2 && \
+	gild/bin/gild checkout gcc $GCC_VER
 
-# git config --global user.email "walkero@gmail.com"
-# git config --global user.name "Georgios Sokianos"
-# git submodule init && \
-# 	git submodule update && \
-# 	gild/bin/gild checkout binutils 2.23.2 && \
-# 	gild/bin/gild checkout gcc $GCC_VER
-
-# # Compile gcc
-# echo "-------- START GCC COMPILATION"
+# Compile gcc
+echo "-------- START GCC COMPILATION"
 # make -C native-build gcc-cross CROSS_PREFIX=/opt/ppc-amigaos -j$(nproc)
