436a8edc20ca44dba3d56334d3f61f18de186246
George Sokianos <walkero@gmail.com>
2024-11-25 09:10:52 +0000

    Added texinfo 7.0 compilation script

---
+++ ppc-amigaos/scripts/compile-texinfo.sh
@@ -0,0 +1,9 @@
+#!/usr/bin/bash
+
+cd /tmp
+
+curl -fsSL "https://ftp.gnu.org/gnu/texinfo/texinfo-7.0.tar.gz" -o /tmp/texinfo.tar.gz && \
+	tar -xzf texinfo.tar.gz && \
+	cd texinfo-7.0 && \
+	./configure --prefix=/usr/local && \
+	make && make install
