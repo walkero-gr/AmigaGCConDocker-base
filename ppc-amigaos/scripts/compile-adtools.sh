d7f9a9c7b67740c41bbbc8e39853de5049231164
George Sokianos <walkero@gmail.com>
2025-10-22 10:57:10 +0100

    A few changes in comiplation scripts

--- ppc-amigaos/scripts/compile-adtools.sh
+++ ppc-amigaos/scripts/compile-adtools.sh
@@ -2,12 +2,17 @@
 
 cd /opt/adtools
 
+BINUTILS=2.40
+if [ "$GCC_VER" = "6" ]; then
+BINUTILS=2.23.2
+fi
+
 git config --global advice.detachedHead false
 git config --global user.email "walkero@gmail.com"
 git config --global user.name "Georgios Sokianos"
 git submodule init && \
 	git submodule update && \
-	gild/bin/gild checkout binutils 2.40 && \
+	gild/bin/gild checkout binutils $BINUTILS && \
 	gild/bin/gild checkout gcc $GCC_VER
 
 \cp /opt/misc/native-build/makefile /opt/adtools/native-build/makefile
@@ -15,4 +20,4 @@ git submodule init && \
 
 # Compile gcc
 echo "-------- START GCC COMPILATION"
-make -C native-build gcc-cross CROSS_PREFIX=/opt/ppc-amigaos BINUTILS_VERSION=2.40 -j$(nproc)
+make -C native-build gcc-cross CROSS_PREFIX=/opt/ppc-amigaos BINUTILS_VERSION=$BINUTILS -j$(nproc --ignore=1)
